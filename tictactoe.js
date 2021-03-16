9882953f9a98405c2d32025838f39a7e4fb8514f
pradhumnarajput <pradhumnarajput@gmail.com>
2021-03-17 01:10:52 +0530

    My tic tac toe game

--- tictactoe.js
+++ tictactoe.js
@@ -1,6 +1,4 @@
-/* Play Tic Toe, against a friend or the computer.
-If u choose to play against the computer, the 'Computer Player' will use the getBestMove function to choose one of the best possible moves.
-*/
+
 
 // -- Constants --
 const SYMBOLS = {
